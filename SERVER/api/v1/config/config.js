d3a2c15fabce1ddb16ae2a4dac7cb411a1b7113f
Moise1 <moiseri01@gmail.com>
2019-06-21 01:50:05 +0200

    changes

--- SERVER/api/v1/config/config.js
+++ SERVER/api/v1/config/config.js
@@ -6,15 +6,15 @@ import prodKeys from './prod';
 
 dotenv.config();
 
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === 'production') {
     module.exports = prodKeys;
   }
-  else if (process.env.NODE_ENV === "test") {
+  else if (process.env.NODE_ENV === 'test') {
     module.exports = testsKeys;
   } else {
     module.exports = devKeys;
   }
   
-  console.log("====The Env=====",process.env.NODE_ENV);
+  // console.log(process.env.NODE_ENV);
   // console.log(process.env.SECRET_OR_PUBLIC_KEY);
   // console.log(process.env.DATABASE_URL);
